73635dac857b2e289192ceed199ce5bf3e63c103
Marichka <svitlananester@ukr.net>
2017-09-27 13:40:53 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -1,25 +1,26 @@
-const number= Number(prompt("Number"));
+const number = Number(prompt("Number"));
+
 function fib(n) {
-  let a = 0;
-  let b = 1;
-  for (let i = 3; i <= n; i++) {
-    let c = a + b;
-    a = b;
-    b = c;
-  }
-  return b;
+    let a = 1;
+    let b = 1;
+    for (let i = 3; i <= n; i++) {
+        let c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
 }
 
-alert( fib(number) ); 
+alert(fib(number));
 
 
 function fib(n) {
-    let mas=[0, 1];
+    let mas = [1, 1];
 
-      for (let i=2; i<=n;i++){
-        mas[i]=mas[mas.length-1]+mas[mas.length-2];
+    for (let i = 2; i <= n; i++) {
+        mas[i] = mas[mas.length - 1] + mas[mas.length - 2];
     }
     return mas[n];
 }
 
-alert( fib(number) );
+alert(fib(number));
